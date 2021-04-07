e82f1b586be9913cec3b7210174e771365b0bfbf
RuslanMirov <ruslan777mirov@gmail.com>
2021-04-07 11:13:49 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ class App extends Component {
       const smartFunds = await getFundsList()
       this.props.MobXStorage.initSFList(smartFunds)
       // view current registry address
-      console.log("SmartFundRegistryADDRESS: ", SmartFundRegistryADDRESS, "!_version 06/04/21_!")
+      console.log("SmartFundRegistryADDRESS: ", SmartFundRegistryADDRESS, "version 07/04/21")
       this.setState({ isDataLoad: true })
     }
   }
